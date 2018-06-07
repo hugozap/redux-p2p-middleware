4b1c49b1d3076e299951ecf5ea9d7cf9e4a8efb6
Hugo Zapata <hugozap@gmail.com>
2018-06-07 17:42:05 -0500

    differentiate local vs replicated actions to avoid re sending to transport layer

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import uuid from "uuid";
 
 export default function replicateActions(allowedActions, transport) {
-    const localActions = [];
+    const locallyDispatched = [];
     let storeRef = null;
 
     //Listen to actions coming from the transport
     transport.on('action', actionMsg => {
 
         //Only process non local actions
-        if(!localActions.indexOf(actionMsg.id) >= 0) {
+        if(!locallyDispatched.indexOf(actionMsg.id) >= 0) {
             if(allowedActions.indexOf(actionMsg.action.type) >= 0) {
+                //Store the replication id so we can check
+                //in the middleware and don't replicate it again
+                actionMsg.action.__replicationId = actionMsg.id
                 storeRef.dispatch(actionMsg.action);
             }
         }
@@ -21,9 +24,15 @@ export default function replicateActions(allowedActions, transport) {
             return action => {
                 if (allowedActions.indexOf(action.type) >= 0) {
                     //Was action originated locally?
-                    if (!action._replicated) {
+                    if (action.__replicationId) {
+                        //replicated action
+                        locallyDispatched.push(action.__replicationId);
+
+                    } else {
+                        //locally dispatched, generate new replication id
                         const id = uuid.v4();
-                        localActions.push(id);
+                        locallyDispatched.push(id);
+                        //send to transport layer
                         transport.replicate({
                             id: id,
                             action: action
